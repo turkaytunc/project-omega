f17db37ef3917d6e274f13c84e33cc282ae3ed01
Turkay Tunc <turkaytunc00@gmail.com>
2020-11-08 13:39:09 +0300

    add search by id

--- src/components/PostContainer/post/Post.jsx
+++ src/components/PostContainer/post/Post.jsx
@@ -1,14 +1,27 @@
 import './post.scss';
 
 import { SinglePost } from '../single-post/SinglePost';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Post = ({ post }) => {
   const [search, setSearch] = useState('');
+  const [filteredSearch, setFilteredSearch] = useState();
 
   const handleInput = (e) => {
     setSearch(e.target.value);
   };
+
+  useEffect(() => {
+    if (search.length > 1) {
+      console.log('3ten fazla');
+      const filterSearch = post.posts.filter((e) => e.id === parseInt(search));
+      setFilteredSearch(filterSearch);
+    }
+  }, [search]);
+
+  useEffect(() => {
+    console.log(filteredSearch);
+  }, [filteredSearch]);
 
   return (
     <div className="post-container">
@@ -19,9 +32,9 @@ export const Post = ({ post }) => {
         placeholder="Search"
         onChange={(e) => handleInput(e)}
       />
-      {post.posts.map((e) => (
-        <SinglePost key={e.id} post={e} />
-      ))}
+      {search.length > 1
+        ? filteredSearch.map((e) => <SinglePost key={e.id} post={e} />)
+        : post.posts.map((e) => <SinglePost key={e.id} post={e} />)}
     </div>
   );
 };
